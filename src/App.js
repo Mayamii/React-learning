1c7e3d8342dfd729d1a08cbc1ae8621b69e92459
Marika <marika.singer@txs.de>
2021-08-24 14:28:58 +0200

    Bis Hooks Q&A

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-
 import React from "react";
-import ReactDOM from "react-dom"
-import Pet from "./Pet"
-import SearchParams from "./SearchParams"
-
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
 // const App = () => {
 //   return React.createElement("div", {}, [
@@ -26,13 +23,13 @@ import SearchParams from "./SearchParams"
 //   ]);
 // };
 
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
 
-const App =() =>{
-   return( 
-   <div>
-<h1>Adopt Me!</h1>
-<SearchParams/>
-    </div>)
-}
-
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
